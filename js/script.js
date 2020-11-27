967429bef13159ceb4b393f571ef29ebb4de1f23
Jason Duquain <jduquain@gmail.com>
2020-11-27 07:09:12 -0500

    progress on varinees site

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+
+gsap.registerPlugin(DrawSVGPlugin);
+gsap.registerPlugin(ScrollTrigger)
+
 const heroSVG = document.querySelector('.hero__svg-icons')
 
 const timeline = gsap.timeline({repeat:-1});
@@ -18,12 +22,76 @@ timeline.to(heroSVG, 3, {
 
 });
 
-document.addEventListener('click', (e) => {
-  console.log(e.target)
-})
-
-
 
 
 /* use onComplete to reset from start and begin again? */
 /* check staggered staggers with no gap lesson! */
+
+
+const afterImg = document.querySelector('.secrets__img--two')
+const range = document.querySelector('.secrets__range');
+
+document.addEventListener('input', (e) => {
+  afterImg.style.width = `${e.target.value}%`;
+  console.log(e.target.value)
+})
+
+const secretsSvg = [...document.querySelectorAll('.secrets__svg path')];
+const secretsSvgTwo = [...document.querySelectorAll('.secrets__svg--two path')];
+const secretsSvgThree = [...document.querySelectorAll('.secrets__svg--three path')];
+
+const newArr = [secretsSvg, secretsSvgTwo, secretsSvgThree];
+
+
+
+
+// secretsSvg.forEach(function(el) {
+//   el.style.stroke = 'black';
+//   el.style.strokeWidth= "3"
+//   el.style.fill = 'none'
+//   el.style.fillOpacity = 0
+// })
+
+// secretsSvgTwo.forEach(function(el) {
+//   el.style.stroke = 'black';
+//   el.style.strokeWidth= "3"
+//   el.style.fill = 'none'
+//   el.style.fillOpacity = 0
+// })
+
+// secretsSvgThree.forEach(function(el) {
+//   el.style.stroke = 'black';
+//   el.style.strokeWidth= "3"
+//   el.style.fill = 'none'
+//   el.style.fillOpacity = 0
+// })
+
+newArr.forEach((el, idx) => {
+  el.forEach((el, idx) => {
+    el.style.stroke = 'black';
+  el.style.strokeWidth= "3"
+  el.style.fill = 'none'
+  el.style.fillOpacity = 0
+  })
+  
+  gsap.from(el, {
+    duration: 3,
+    drawSVG: '0%',
+    scrollTrigger: {
+      trigger: el,
+      markers: true,
+      start: 'top 75%',
+      end: 'bottom 25%',
+      toggleActions: 'play none none none'
+    },
+    onComplete: function() {
+      this.targets().forEach(function(el) {
+        el.style.stroke = 'none'
+        el.style.fill = null;
+        el.style.fillOpacity = 1;
+        el.style.transition = 'fill-opacity 2s';
+      })
+    }
+  })
+
+})
