2b08fd4faa0f2352f4ff2fede400af490d985390
Jason Duquain <jduquain@gmail.com>
2020-11-29 17:12:16 -0500

    more progress

--- js/script.js
+++ js/script.js
@@ -99,20 +99,121 @@ newArr.forEach((el, idx) => {
 
 const list = document.querySelector('.services__list');
 const links = document.querySelectorAll('.services__link');
-const mainItems = document.querySelectorAll('.services__pow')
+const mainItems = document.querySelectorAll('.services__pow');
+
+const listTwo = document.querySelectorAll('.services__list--two')
+const linksTwo = document.querySelector('.services__link--two');
+
+console.log(listTwo)
 
 list.addEventListener('click', function(e) {
+  e.preventDefault();
+
   if (!e.target.classList.contains("active") && e.target.classList.contains("services__link")) {
     links.forEach((el, idx) => {
       e.target.classList.add("active");
       el.classList.remove("active");
       mainItems[idx].classList.remove("active");
+      mainItems[idx+4].classList.remove("active");
+      mainItems[idx+5].classList.remove("active");
+      mainItems[idx+6].classList.remove("active");
+      mainItems[idx+7].classList.remove("active");
+      mainItems[idx+8].classList.remove("active");
+      mainItems[idx+9].classList.remove("active");
+      mainItems[idx+10].classList.remove("active");
+      mainItems[idx+11].classList.remove("active");
 
-      if (mainItems[idx].children[1].firstElementChild.textContent === e.target.getAttribute("href").slice(1))  {
-        mainItems[idx].classList.add("active")
-      }
+       if (mainItems[idx].children[2].firstElementChild.textContent === e.target.getAttribute("href").slice(1))  {
+         mainItems[idx].classList.add("active");
+       } 
+
     })
 
-    
   }
 })
+
+listTwo.forEach(function(el, idx) {
+  el.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    
+
+    if (!e.target.classList.contains("active") && e.target.classList.contains("services__link--two")) {
+      el.firstElementChild.firstElementChild.classList.remove("active")
+      el.firstElementChild.nextElementSibling.firstElementChild.classList.remove("active")
+      e.target.classList.add("active");
+    }
+
+    
+
+    mainItems[idx].classList.remove("active")
+
+    if (mainItems[idx+4].children[1].firstElementChild.textContent === e.target.getAttribute("href").slice(1))  {
+      
+      mainItems[idx+4].classList.add("active");
+    } 
+
+    if (mainItems[idx+5].children[1].firstElementChild.textContent === e.target.getAttribute("href").slice(1))  {
+      
+      mainItems[idx+5].classList.add("active");
+    } 
+
+    if (mainItems[idx+6].children[1].firstElementChild.textContent === e.target.getAttribute("href").slice(1))  {
+      
+      mainItems[idx+6].classList.add("active");
+    } 
+
+    if (mainItems[idx+7].children[1].firstElementChild.textContent === e.target.getAttribute("href").slice(1))  {
+      
+      mainItems[idx+7].classList.add("active");
+    } 
+
+    if (mainItems[idx+8].children[1].firstElementChild.textContent === e.target.getAttribute("href").slice(1))  {
+      
+      mainItems[idx+8].classList.add("active");
+    } 
+
+    if (mainItems[idx+9].children[1].firstElementChild.textContent === e.target.getAttribute("href").slice(1))  {
+      
+      mainItems[idx+9].classList.add("active");
+    } 
+
+    if (mainItems[idx+10].children[1].firstElementChild.textContent === e.target.getAttribute("href").slice(1))  {
+      
+      mainItems[idx+10].classList.add("active");
+    } 
+
+    if (mainItems[idx+11].children[1].firstElementChild.textContent === e.target.getAttribute("href").slice(1))  {
+      
+      mainItems[idx+11].classList.add("active");
+    } 
+  })
+})
+
+
+
+const circleOne = document.querySelector('.animate')
+const circleTwo = document.querySelector('.animate--two')
+const textOne = document.querySelector('.text--one')
+
+let el = document.querySelector('.animate');
+let style = window.getComputedStyle(el);
+
+
+var startTime = new Date().getTime();
+
+let interval = setInterval(function() {
+  if (new Date().getTime() - startTime > 2000){
+    clearInterval(interval);
+    return;
+  }
+  let arr = style.getPropertyValue('stroke-dasharray').split(",");
+  textOne.textContent = Number(arr[0].slice(0, 4)) + '%'
+  console.log(arr[0])
+
+  
+   
+}, 50)
+
+
+
