ec9c14c23fc19ffdfe1a8747e546693a43fe3570
Jason Duquain <jduquain@gmail.com>
2020-11-30 16:38:36 -0500

    guess

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 
 gsap.registerPlugin(DrawSVGPlugin);
 gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(SplitText);
 
 const heroSVG = document.querySelector('.hero__svg-icons')
 
@@ -66,35 +67,40 @@ const newArr = [secretsSvg, secretsSvgTwo, secretsSvgThree];
 //   el.style.fillOpacity = 0
 // })
 
-newArr.forEach((el, idx) => {
-  el.forEach((el, idx) => {
-    el.style.stroke = 'black';
-  el.style.strokeWidth= "3"
-  el.style.fill = 'none'
-  el.style.fillOpacity = 0
-  })
-  
-  gsap.from(el, {
-    duration: 3,
-    drawSVG: '0%',
-    scrollTrigger: {
-      trigger: el,
-      markers: true,
-      start: 'top 75%',
-      end: 'bottom 25%',
-      toggleActions: 'play none none none'
-    },
-    onComplete: function() {
-      this.targets().forEach(function(el) {
-        el.style.stroke = 'none'
-        el.style.fill = null;
-        el.style.fillOpacity = 1;
-        el.style.transition = 'fill-opacity 2s';
-      })
-    }
-  })
 
-})
+
+
+
+// newArr.forEach((el, idx) => {
+//   el.forEach((el, idx) => {
+//     el.style.stroke = 'black';
+//   el.style.strokeWidth= "3"
+//   el.style.fill = 'none'
+//   el.style.fillOpacity = 0
+//   })
+  
+//   gsap.from(el, {
+//     duration: 3,
+//     drawSVG: '0%',
+//     scrollTrigger: {
+//       trigger: el,
+//       markers: true,
+//       start: 'top 75%',
+//       end: 'bottom 25%',
+//       toggleActions: 'play none none none'
+//     },
+//     onComplete: function() {
+//       console.log(this.targets())
+//       this.targets().forEach(function(el) {
+//         el.style.stroke = 'none'
+//         el.style.fill = null;
+//         el.style.fillOpacity = 1;
+//         el.style.transition = 'fill-opacity 2s';
+//       })
+//     }
+//   })
+
+// })
 
 
 const list = document.querySelector('.services__list');
@@ -195,25 +201,130 @@ listTwo.forEach(function(el, idx) {
 const circleOne = document.querySelector('.animate')
 const circleTwo = document.querySelector('.animate--two')
 const textOne = document.querySelector('.text--one')
+const textTwo = document.querySelector('.text--two')
 
-let el = document.querySelector('.animate');
-let style = window.getComputedStyle(el);
+doIt(circleOne, textOne);
+doIt(circleTwo, textTwo);
 
 
-var startTime = new Date().getTime();
+function doIt(el, text) {
+  var startTime = new Date().getTime();
 
-let interval = setInterval(function() {
-  if (new Date().getTime() - startTime > 2000){
-    clearInterval(interval);
-    return;
+  let elStyles = window.getComputedStyle(el);
+
+  let interval = setInterval(function() {
+    if (new Date().getTime() - startTime > 2000) {
+      clearInterval(interval);
+      return;
+    }
+    let arr = elStyles.getPropertyValue('stroke-dasharray').split(",");
+
+    text.textContent = Number(arr[0].slice(0, arr[0].length - 1)).toFixed(0) + '%';
+  }, 50)
+
+}
+
+
+let headingPath = document.querySelector('#headingPath');
+let headingLineOne = document.querySelector('#headingLine--one');
+let headingLineTwo = document.querySelector('#headingLine--two');
+
+
+gsap.from([headingPath, headingLineOne, headingLineTwo], {
+  duration: 3,
+  drawSVG: '0%',
+  scrollTrigger: {
+    trigger: [headingPath, headingLineOne, headingLineTwo],
+    markers: true,
+    start: 'top 75%',
+    end: 'bottom 25%',
+    toggleActions: 'play none none none',
+    scrub: 0.1
   }
-  let arr = style.getPropertyValue('stroke-dasharray').split(",");
-  textOne.textContent = Number(arr[0].slice(0, 4)) + '%'
-  console.log(arr[0])
 
-  
-   
-}, 50)
+})
 
 
+const monitor = document.querySelectorAll('#monitor path')
 
+gsap.from(monitor, {
+  duration: 3,
+  drawSVG: '0%',
+  scrollTrigger: {
+    trigger: monitor,
+    markers: true,
+    start: 'top 75%',
+    end: 'bottom 55%',
+    toggleActions: 'play none none none',
+    scrub: 0.1
+  }
+
+})
+
+let svgee = document.querySelector('.viewbox--svg')
+let rects = document.querySelectorAll('.viewbox--svg rect');
+let btnOne = document.querySelector('btn--one');
+let btnTwo = document.querySelector('btn--two');
+let btnThree = document.querySelector('btn--three');
+
+let allBtns = document.querySelectorAll('.btn--one, .btn--two, .btn--three')
+
+allBtns.forEach(el => {
+  el.addEventListener('click', (e) => {
+    gsap.to(svgee, {
+      duration: 2,
+      attr: {
+        viewBox: el.getAttribute('data-num')
+      }
+    })
+  })
+})
+
+let colors = ["#d94362", "black", "gray", "red", "green", "goldenrod", "rebeccapurple", "blue", "papayawhip", "pink", "blue"]
+
+
+rects.forEach((el) => {
+  var randColor = colors[Math.floor(Math.random() * colors.length )]
+  el.style.fill = randColor;
+})
+
+
+let pathOne = document.querySelector('.absolutely path:first-of-type');
+let pathTwo = document.querySelector('.absolutely path:nth-of-type(2)');
+let pText = document.querySelector('.absolutely p')
+
+const splitText = new SplitText(pText, {type: "chars"})
+
+console.log(pathOne, pathTwo)
+
+gsap.from([pathOne, pathTwo], {
+  duration: 1,
+  drawSVG: '0%',
+  scrollTrigger: {
+    trigger: [pathOne, pathTwo],
+    markers: true,
+    markerColor: 'purple',
+    start: 'top 65%',
+    end: 'bottom 65%',
+    toggleActions: 'play none none none',
+    scrub: 0.1
+  }
+
+})
+
+gsap.from(splitText.chars, {
+  duration: 1,
+  y: 50,
+  ease: "back",
+  stagger: 0.7,
+  scrollTrigger: {
+    trigger: splitText.chars,
+    markers: true,
+    markerColor: 'purple',
+    start: 'top 65%',
+    end: 'bottom 65%',
+    toggleActions: 'play none none none',
+    scrub: 0.3
+  }
+
+})
